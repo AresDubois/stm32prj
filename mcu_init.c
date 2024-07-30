3868caf726a7f16b96b05dc30bdc6e2499d2ae97
User_666 <108924075+AresDubois@users.noreply.github.com>
2024-07-30 19:57:31 +0400

    Add files via upload

---
+++ mcu_init.c
@@ -0,0 +1,11 @@
+#IFNDEF _REGBASE_
+#define _REGBASE_
+#include "regbase.h"
+#ENDIF
+
+#include "mcu_init.h"
+
+void mcu_init(void){
+  //
+}
+
---
+++ time_manager.c
@@ -0,0 +1,10 @@
+#include "regbase.h"
+#include <stdio.h>
+#include "mcu_init.h"
+
+
+int main(void) {
+  
+  
+  return 0;
+}
